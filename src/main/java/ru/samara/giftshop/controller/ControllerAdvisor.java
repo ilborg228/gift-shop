61a7a926864c5fb0c16bf314ccdfe44b6c9daa5d
ilborg228 <asd120200@gmail.com>
2021-11-01 21:42:24 +0400

    finish up ControllerAdvisor.java

--- src/main/java/ru/samara/giftshop/controller/ControllerAdvisor.java
+++ src/main/java/ru/samara/giftshop/controller/ControllerAdvisor.java
@@ -5,9 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import ru.samara.giftshop.exceptions.ApiException;
-import ru.samara.giftshop.exceptions.NoSuchCategoryException;
-import ru.samara.giftshop.exceptions.NoSuchProductException;
+import ru.samara.giftshop.exceptions.*;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -15,7 +13,8 @@ import java.time.ZonedDateTime;
 @RestControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {NoSuchCategoryException.class, NoSuchProductException.class})
+    @ExceptionHandler(value = {NoSuchCategoryException.class, NoSuchProductException.class,
+            ProductAlreadyExistException.class, CategoryAlreadyExistException.class})
     public ResponseEntity<?> handleCityNotFoundException(RuntimeException ex) {
 
         ApiException apiException = new ApiException(ex.getMessage(),ex,
